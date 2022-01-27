63260816a305b8f9a69ca1b388d01e81869d5912
Nerto Kageh <kagehnerto@yahoo.com>
2022-01-27 10:17:56 -0600

    Initial Commit

---
+++ Assignment Notebook Attempt 3/Assignment_Notebook_Attempt_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_Attempt_3App.swift
+//  Assignment Notebook Attempt 3
+//
+//  Created by Student on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_Attempt_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
